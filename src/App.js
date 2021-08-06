335bee02de1327c4fa554da401ffef3bf44c2ebd
Archit Badjatya <architj240@gmail.com>
2021-08-06 07:36:19 +0530

    using axios for api calls and storing data in App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 
+// Importing axios
+import axios from "axios";
+
 // Importing Components
 import Navbar from "./App/components/layouts/Navbar/Navbar.component";
 import Users from "./App/components/users/Users/Users.component";
 
 export class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const response = await axios.get("https://api.github.com/users");
+    this.setState({ users: response.data, loading: false });
+  }
+
   render() {
     return (
       <div>
         <Navbar />
-        <Users />
+        <Users users={this.state.users} loading={this.state.loading} />
       </div>
     );
   }
--- src/App/components/users/Users/Users.component.js
+++ src/App/components/users/Users/Users.component.js
@@ -15,38 +15,10 @@ const UsersContainer = styled.div`
 `;
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 1,
-        login: "Mojombo",
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo",
-      },
-      {
-        id: 2,
-        login: "defunkt",
-        avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunk",
-      },
-      {
-        id: 3,
-        login: "pjhyett",
-        avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/pjhyett",
-      },
-      {
-        id: 4,
-        login: "pjhyett",
-        avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/pjhyett",
-      },
-    ],
-  };
   render() {
     return (
       <UsersContainer>
-        {this.state.users.map((user) => (
+        {this.props.users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </UsersContainer>
