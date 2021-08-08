c996d5da1a03dee48c60705e3adf6a57005f5f32
Archit Badjatya <architj240@gmail.com>
2021-08-08 10:42:25 +0530

    Style: AboutPage

--- src/App/pages/About/AboutPage.js
+++ src/App/pages/About/AboutPage.js
@@ -1,10 +1,42 @@
 import React from "react";
 
+// Importing Img
+import AboutImage from "../../assets/img/About.svg";
+
+//Importing Styles
+import {
+  AboutContainer,
+  LeftColumn,
+  RightColumn,
+  Img,
+  Heading,
+  Paragraph,
+} from "./Aboutpage.styles";
+
 const AboutPage = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-    </div>
+    <AboutContainer>
+      <LeftColumn>
+        <Img src={AboutImage} alt="About Page Image" />
+      </LeftColumn>
+      <RightColumn>
+        <Heading>About</Heading>
+        <Paragraph>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla, ullam
+          saepe eius nostrum facere dolorem aperiam qui beatae ea ab odio. Amet
+          eos, molestiae quo mollitia velit enim ipsa ipsum.
+        </Paragraph>
+        <Paragraph>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla, ullam
+          saepe eius nostrum facere dolorem aperiam qui beatae ea ab odio. Amet
+          eos, molestiae quo mollitia velit enim ipsa ipsum.
+        </Paragraph>
+        <Paragraph>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero autem
+          vitae nobis. Debitis, amet dolore.
+        </Paragraph>
+      </RightColumn>
+    </AboutContainer>
   );
 };
 
---
+++ src/App/pages/About/Aboutpage.styles.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import Colors from "../../constants/Colors";
+
+export const AboutContainer = styled.div`
+  width: 1024px;
+  max-width: 1024px;
+  margin: 7% auto 5% auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const LeftColumn = styled.div`
+  width: 40%;
+  height: 450px;
+`;
+
+export const RightColumn = styled.div`
+  width: 50%;
+  height: 450px;
+`;
+
+export const Img = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+export const Heading = styled.p`
+  font-size: 1.3rem;
+  font-weight: 600;
+  color: ${Colors.primary};
+  text-transform: uppercase;
+`;
+
+export const Paragraph = styled.p`
+  font-size: 1rem;
+  color: #758283;
+  font-weight: 400;
+`;
